ee89c7a99cfa6fb2f628e6dc14ad099339fb4c3e
Amirsz01 <amir.rima@bk.ru>
2021-12-03 02:03:59 +0300

    update

--- assets/controllers/mydropzone_controller.js
+++ assets/controllers/mydropzone_controller.js
@@ -4,16 +4,16 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     connect() {
-        this.element.addEventListener('dropzone-multiple:connect', this._onConnect);
-        this.element.addEventListener('dropzone-multiple:change', this._onChange);
-        this.element.addEventListener('dropzone-multiple:clear', this._onClear);
+        this.element.addEventListener('dropzone:connect', this._onConnect);
+        this.element.addEventListener('dropzone:change', this._onChange);
+        this.element.addEventListener('dropzone:clear', this._onClear);
     }
 
     disconnect() {
         // You should always remove listeners when the controller is disconnected to avoid side-effects
-        this.element.removeEventListener('dropzone-multiple:connect', this._onConnect);
-        this.element.removeEventListener('dropzone-multiple:change', this._onChange);
-        this.element.removeEventListener('dropzone-multiple:clear', this._onClear);
+        this.element.removeEventListener('dropzone:connect', this._onConnect);
+        this.element.removeEventListener('dropzone:change', this._onChange);
+        this.element.removeEventListener('dropzone:clear', this._onClear);
     }
 
     _onConnect(event) {
