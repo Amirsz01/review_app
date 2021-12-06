297c189b140c91cdf19b3444b6ce02ca61b9417d
Amirsz01 <amir.rima@bk.ru>
2021-12-06 19:04:07 +0300

    Swith locale

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -22,7 +22,6 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 class ReviewController extends AbstractController
 {
